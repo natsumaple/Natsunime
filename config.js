606ccbd79775a506fd16f6d00cf09a0b4c738e6f
natsumaple <natsu2yasumi@gmail.com>
2026-06-29 17:27:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDkLd6zLlK5VihMLuTd3bmHO5AYVVyBFy8",
+  authDomain: "natsunime-86e9d.firebaseapp.com",
+  projectId: "natsunime-86e9d",
+  storageBucket: "natsunime-86e9d.firebasestorage.app",
+  messagingSenderId: "804368157744",
+  appId: "1:804368157744:web:6c5760431f530accf3f1f5",
+  measurementId: "G-DZTTZTP027"
+};
+
+export const APP_CONFIG = {
+  LOGO_URL: "https://i.ibb.co.com/93gtj8vP/e38097d923934c9595cd26668ed062f2-webp.webp",
+  DEFAULT_AVATAR: "https://i.ibb.co.com/Kz6fKRpr/51c359defeb3cbae892c5cdada9ab747-webp.webp",
+  GACHA_COST_X1: 150,
+  GACHA_COST_X10: 1350
+};
